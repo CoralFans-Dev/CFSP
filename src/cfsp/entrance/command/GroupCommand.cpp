3d16ef0c4405ca12fa557df02efb87074348a9ab
Eragon327 <2287162120@qq.com>
2026-03-05 22:26:43 +0800

    added enum for lookat operate

--- src/cfsp/entrance/command/GroupCommand.cpp
+++ src/cfsp/entrance/command/GroupCommand.cpp
@@ -430,6 +430,35 @@ void ComandManager::registerGroupComand() {
             for (auto perRes : res) perRes.output(output);
         });
 
+    // sp g lookat <gname: cfspGroup> <facing: cfspFacing>
+    ll ::command ::CommandRegistrar ::getInstance(false).tryRegisterRuntimeEnum(
+        "cfspFacing",
+        {
+            {"north", 0},
+            {"south", 1},
+            {"west",  2},
+            {"east",  3},
+            {"up",    4},
+            {"down",  5}
+    }
+    );
+    this->command->runtimeOverload()
+        .text("g")
+        .text("lookat")
+        .required("gname", ll::command::ParamKind::SoftEnum, "cfspGroup")
+        .required("facing", ll::command::ParamKind::Enum, "cfspFacing")
+        .execute([this](CommandOrigin const& origin, CommandOutput& output, ll::command::RuntimeCommand const& self) {
+            auto player = this->tryGetPlayer(origin);
+            if (!player.has_value()) return output.error("command.fail.illegalOrigin"_tr());
+            std::vector<base::OperateResult> res;
+            res = manager::CFSPManager::getInstance().groupLookAt(
+                player.value(),
+                self["gname"].get<ll::command::ParamKind::SoftEnum>(),
+                static_cast<simulated_player::SimPlayer::Direction>(self["facing"].get<ll::command::ParamKind::Enum>().index)
+            );
+            for (auto perRes : res) perRes.output(output);
+        });
+
     // sp g <moveto|navto> <gname: cfspGroup> [pos: Vec3] [speed: float]
     ll::command::CommandRegistrar::getInstance(false).tryRegisterRuntimeEnum(
         "cfspGroupOperate7",
