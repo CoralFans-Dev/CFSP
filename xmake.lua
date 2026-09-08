9fface6e133b6f82d1a0699bd97a8343f43467a8
kuangby <3091240752@qq.com>
2026-09-08 20:14:41 +0800

    update xmake.lua

--- xmake.lua
+++ xmake.lua
@@ -3,7 +3,7 @@ add_rules("mode.debug", "mode.release")
 add_repositories("liteldev-repo https://github.com/LiteLDev/xmake-repo.git")
 add_repositories("coralfansdev-repo https://github.com/CoralFans-Dev/xmake-repo.git")
 
-add_requires("levilamina", {configs = {target_type = get_config("target_type")}})
+add_requires("levilamina 26.32.2", {configs = {target_type = get_config("target_type")}})
 
 add_requires(
     "levibuildscript",
